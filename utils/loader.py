08fc108830ced4930d7aacb9db695139286b77a8
Viswajit Kumar <nijlogan@gmail.com>
2024-04-03 16:16:07 -0400

    Base 100-line implementation + loader

---
+++ utils/loader.py
@@ -0,0 +1,51 @@
+import torch
+from tqdm import tqdm
+
+from datasets import dataset_dict
+
+
+def load_data(dataset_name, split='train'):
+    if dataset_name not in dataset_dict.keys():
+        raise Exception('Invalid dataset name')
+
+    dir_dict = {'blender': './../data/nerf_synthetic/lego',
+                'llff': './../data/nerf_llff_data/fern'}
+
+    wh_dict = {'blender': tuple([400, 400]),
+               'llff': tuple([504, 378])}
+
+    kwargs = {'root_dir': dir_dict[dataset_name],
+              'split': split,
+              'img_wh': wh_dict[dataset_name]}
+
+    if dataset_name == 'llff':
+        kwargs['spheric_poses'] = True
+
+    dataset = dataset_dict[dataset_name](**kwargs)
+
+    out_set = torch.zeros((1, 9))
+
+    if split == 'train':
+        out_set = torch.zeros((len(dataset), 9))
+
+        for i in tqdm(range(len(dataset))):
+            sample = dataset[i]
+
+            ray_org_dir = torch.Tensor(sample['rays'][0:6])
+            gt_pixel_color = torch.Tensor(sample['rgbs'])
+
+            out_set[i] = torch.cat((ray_org_dir, gt_pixel_color))
+
+    elif split == 'test':
+        out_set = torch.zeros((len(dataset) * len(dataset[0]['rays']), 9))
+
+        for i in tqdm(range(len(dataset))):
+            group = dataset[i]
+
+            for j in range(len(group['rays'])):
+                ray_org_dir = torch.Tensor(group['rays'][j][0:6])
+                gt_pixel_color = torch.Tensor(group['rgbs'][j])
+
+                out_set[len(dataset) * i + j] = torch.cat((ray_org_dir, gt_pixel_color))
+
+    return out_set
